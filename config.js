e8871eb2251d5b762f269f84bd9f02204d48d9fb
seguridad-ai <sig@explodrillingperu.pe>
2026-09-18 12:24:01 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -7,4 +7,18 @@
 window.SSOMAC_CONFIG = {
   SUPABASE_URL: 'https://fnihvppboqrbazhiubau.supabase.co',
   SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_Ld_A2Y5gA3kuaZk56NvByQ_jojgNEkk'
+
+// EMAILJS
+  EMAILJS_SERVICE_ID:
+    'service_x350i0i',
+
+  EMAILJS_TEMPLATE_ID:
+    'template_y8msygb',
+
+  EMAILJS_PUBLIC_KEY:
+    'mDQxO50RMcWqBm-K0',
+
+  // URL PUBLICA DEL SISTEMA
+  APP_URL:
+    'https://seguridad-ai.github.io/SSOMAC-Reportes'
 };
