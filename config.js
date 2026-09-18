633f84a2fc2b48b4627240592f608ce6f0000751
seguridad-ai <sig@explodrillingperu.pe>
2026-09-18 12:43:27 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,18 @@
 // ============================================================
 // CONFIGURACIÓN DEL PROYECTO SUPABASE
 // ============================================================
-// Pega aquí tu Publishable key.
-// NO uses Secret key ni service_role.
+// NO usar Secret key ni service_role.
 
 window.SSOMAC_CONFIG = {
-  SUPABASE_URL: 'https://fnihvppboqrbazhiubau.supabase.co',
-  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_Ld_A2Y5gA3kuaZk56NvByQ_jojgNEkk'
 
-// EMAILJS
+  // SUPABASE
+  SUPABASE_URL:
+    'https://fnihvppboqrbazhiubau.supabase.co',
+
+  SUPABASE_PUBLISHABLE_KEY:
+    'sb_publishable_Ld_A2Y5gA3kuaZk56NvByQ_jojgNEkk',
+
+  // EMAILJS
   EMAILJS_SERVICE_ID:
     'service_x350i0i',
 
@@ -18,7 +22,8 @@ window.SSOMAC_CONFIG = {
   EMAILJS_PUBLIC_KEY:
     'mDQxO50RMcWqBm-K0',
 
-  // URL PUBLICA DEL SISTEMA
+  // URL PÚBLICA DEL SISTEMA
   APP_URL:
     'https://seguridad-ai.github.io/SSOMAC-Reportes'
+
 };
